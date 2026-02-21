a7989bb98bdaf1cdd5f5dd458414bdd90c0d1f17
Mudit Kumar Singh <muditksingh96184@gmail.com>
2026-02-21 22:27:25 +0530

    F

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://expensetracker-api-qyqu.onrender.com";
+export const BASE_URL = "https://expense-tracker-api-pi-seven.vercel.app";
 
 export const API_PATHS = {
     AUTH: {
